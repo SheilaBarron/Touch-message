e88ff0ea68061c48d4a7e1da1f2f4923a62b0886
Marie-Charlotte <cottereau@telecom-paristech.fr>
2020-06-22 22:44:52 +0200

    comment the ChatMessage.java

--- Module Android/app/src/main/java/com/example/a92gde/chatapp/ChatMessage.java
+++ Module Android/app/src/main/java/com/example/a92gde/chatapp/ChatMessage.java
@@ -1,12 +1,13 @@
 package com.example.a92gde.chatapp;
 
-
+//Class to fill the content of the ChatArrayAdapter
 public class ChatMessage {
     public boolean left;
     public String message;
 
     public ChatMessage(boolean left, String message) {
         super();
+        //determine if the message belong to the left or the right of the screen
         this.left = left;
         this.message = message;
     }
